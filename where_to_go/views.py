814c1d53419684f4fd8e5e3cdb2ddc842aee8fe7
Pavelwell7 <pavelbugaev303@gmail.com>
2026-05-03 10:37:27 +0300

    Move data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,4 +1,29 @@
+import json
 from django.shortcuts import render
+from places.models import Place
+
+import json
+from django.shortcuts import render
+from places.models import Place
+
 
 def show_map(request):
-    return render(request, 'index.html')
+    places = Place.objects.all()
+    geojson = {
+        'type': 'FeatureCollection',
+        'features': [
+            {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [place.lng, place.lat],
+                },
+                'properties': {
+                    'title': place.title,
+                    'placeId': place.id,
+                },
+            }
+            for place in places
+        ],
+    }
+    return render(request, 'index.html', {'geojson': geojson})
