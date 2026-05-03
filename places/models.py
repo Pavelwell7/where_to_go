a2ea01f81bb320535057ccb5e631943ed5009765
Pavelwell7 <pavelbugaev303@gmail.com>
2026-05-03 10:14:02 +0300

    Setup media files serving

--- places/models.py
+++ places/models.py
@@ -9,3 +9,13 @@ class Place(models.Model):
   def __str__(self):
     return self.title
 
+class Image(models.Model):
+  place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
+  image = models.ImageField('Картинка')
+  order = models.PositiveIntegerField('Порядок', default=0)
+
+  class Meta:
+    ordering = ['order']
+
+  def __str__(self):
+    return f'{self.place.title} — {self.order}'
