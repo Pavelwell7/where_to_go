5c047f2d57df12fe0ec122648a0bbd4703b047b7
Pavelwell7 <pavelbugaev303@gmail.com>
2026-05-03 20:01:43 +0300

    load json

--- places/views.py
+++ places/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from .models import Place
 
-# Create your views here.
+
+def place_details(request, place_id):
+  place = get_object_or_404(Place, id=place_id)
+
+  json = {
+    'title': place.title,
+    'imgs': [image.image.url for image in place.images.all()],
+    'description': place.description,
+  }
+
+  return JsonResponse(json, json_dumps_params={'indent': 2, 'ensure_ascii': False})
