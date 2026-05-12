2416f09ffb73290a0ea72de182fb17f94e84b88d
Pavelwell7 <pavelbugaev303@gmail.com>
2026-05-12 18:58:03 +0300

    Update load_place.py

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -24,8 +24,8 @@ class Command(BaseCommand):
         place, created = Place.objects.get_or_create(
             title=place_data['title'],
             defaults={
-                'description_short': place_data.get('description_short', ''),
-                'description_long': place_data.get('description_long', ''),
+                'short_description': place_data.get('short_description', ''),
+                'long_description': place_data.get('long_description', ''),
                 'lat': place_data['coordinates']['lat'],
                 'lng': place_data['coordinates']['lng'],
             }
