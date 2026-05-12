f2d0c962b81c81343fc931d3dc6cdf63bad3d609
Pavelwell7 <pavelbugaev303@gmail.com>
2026-05-12 18:53:33 +0300

    Update settings.py

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1'])
 
 
 INSTALLED_APPS = [
